4c9255e25f593460ab8bd39a7ca8ea08ef2edcff
Adam Korzeniak <adamak1990@gmail.com>
2016-02-15 20:48:40 +0100

    recreated

--- Minesweeper/src/Minesweeper.java
+++ Minesweeper/src/Minesweeper.java
@@ -115,7 +115,7 @@ public class Minesweeper {
 	}
 
 	public void loadGame() {
-		ArrayList<NewButton> buttons = new ArrayList<NewButton>();
+		buttons = new ArrayList<NewButton>();
 		try {
 			FileInputStream fileStream = new FileInputStream("Game.sav");
 			ObjectInputStream is = new ObjectInputStream(fileStream);
@@ -139,8 +139,12 @@ public class Minesweeper {
 		field = new JPanel(grid);
 		for (NewButton a: buttons) {
 			field.add(a);
+			if(a.isFlagged()){
+				fl = new FlagListener();
+				a.addMouseListener(fl);
+			} else {
 			bl = new ButtonListener();
-			a.addMouseListener(bl);
+			a.addMouseListener(bl);}
 		}
 		left = saveGame.left;
 		minesAtStart = saveGame.minesAtStart;
@@ -287,6 +291,7 @@ public class Minesweeper {
 	}
 
 	public void reveal(NewButton b) {
+		System.out.println(buttons.indexOf(b));
 		if (b.isMine) {
 			bust();
 		} else if (b.bombNeighbours > 0) {
@@ -296,13 +301,13 @@ public class Minesweeper {
 				left--;
 			}
 		} else {
-			if (b.isEnabled() && !b.isFlagged()) {
+			if (b.isEnabled()) {
 				b.setEnabled(false);
 				left--;
 			}
 			neighbours(buttons.indexOf(b));
 			for (int a : neighbours) {
-				if (buttons.get(a).isEnabled() && !buttons.get(a).isFlagged()) {
+				if (buttons.get(a).isEnabled()) {
 					buttons.get(a).setEnabled(false);
 					left--;
 					if (buttons.get(a).bombNeighbours > 0) {
@@ -316,10 +321,13 @@ public class Minesweeper {
 	}
 
 	public void bust() {
+		System.out.println("done");
 		for (NewButton b : buttons) {
 			b.setEnabled(false);
 			if (b.isMine) {
 				b.setText("X");
+			} else if (b.bombNeighbours>0){
+				b.setText(Integer.toString(b.bombNeighbours));
 			}
 		}
 		JOptionPane.showOptionDialog(null, "Przegra³eœ, Ty Lamusie", "Przegrana", JOptionPane.DEFAULT_OPTION,
@@ -368,7 +376,6 @@ public class Minesweeper {
 	public void neighbours(int index) {
 		int x = getX(index);
 		int y = getY(index);
-		if(neighbours.isEmpty())
 		neighbours = new ArrayList<Integer>();
 		if (x > 1 && x < width && y > 1 && y < height) {
 			neighbours.add(index + width + 1);
